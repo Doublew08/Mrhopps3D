efd99c8c6ec8c1b1cc5e9e01ccd64185deffefc6
AhmedGamal227 <79380871+AhmedGamal227@users.noreply.github.com>
2021-07-18 17:18:43 +0200

    RabbitManagerInRoom1

---
+++ MrHopp_3D(2020)/Assets/Scripts/RabbitActive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitActive : MonoBehaviour
+{
+     public GameObject Rabbit1;
+    public GameObject Rabbit2;
+     public int CurrentRabbit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(Random.Range(0, 2));
+        CurrentRabbit = Random.Range(0, 2);
+        if (CurrentRabbit == 0)
+        {
+            Rabbit1.SetActive(true);
+            Rabbit2.SetActive(false);
+        }
+        if (CurrentRabbit == 1)
+        {
+            Rabbit2.SetActive(true);
+            Rabbit1.SetActive(false);
+        }
+        Debug.Log("Currrent Rabbit is :" + CurrentRabbit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
