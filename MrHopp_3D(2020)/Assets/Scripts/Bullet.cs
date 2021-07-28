5053b6bf89de9d145dbedab7ccd343a32b55be70
AhmedGamal227 <79380871+AhmedGamal227@users.noreply.github.com>
2021-07-28 23:07:23 +0200

    Add Bullet For The Gun

---
+++ MrHopp_3D(2020)/Assets/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+     GameObject bullet;
+    public GameObject BulletPrefab;
+    private float NextBullet;
+    public float FireRate;
+    public Transform SpawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time > NextBullet)
+            {
+                NextBullet = FireRate + Time.time;
+
+                bullet = Instantiate(BulletPrefab, SpawnPoint.position, Quaternion.identity);
+                bullet.gameObject.GetComponent<Rigidbody>().velocity = transform.forward * 100f;
+            }
+            Debug.Log("Shooting");
+            StartCoroutine(destroy());
+            
+        }
+    }
+
+    IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(bullet);
+    }
+}
--- MrHopp_3D(2020)/Assets/Scripts/PlayerShooting.cs
+++ MrHopp_3D(2020)/Assets/Scripts/PlayerShooting.cs
@@ -16,7 +16,7 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
             PlayerAnimator.SetBool("Shoot", true);
         }
