6b90b53781ef6af4db37175604c6597523ad8da0
AhmedGamal227 <79380871+AhmedGamal227@users.noreply.github.com>
2021-07-28 21:47:56 +0200

    ysta shof TestingAi Scene

---
+++ MrHopp_3D(2020)/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    private Animator PlayerAnimator;
+    public int FireRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            PlayerAnimator.SetBool("Shoot", true);
+        }
+        else
+        {
+            PlayerAnimator.SetBool("Shoot", false);
+        }
+    }
+}
