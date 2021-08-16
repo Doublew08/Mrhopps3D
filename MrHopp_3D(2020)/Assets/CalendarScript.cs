0ad1d21242e7a2d034ab4321c4a9893a1b499050
Doublew08 <86314168+Doublew08@users.noreply.github.com>
2021-08-16 16:16:04 +0200

    Done Calendar date showing

---
+++ MrHopp_3D(2020)/Assets/CalendarScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalendarScript : MonoBehaviour
+{
+    float beg_door;
+    float End_door;
+    public GameObject Date;
+    // Start is called before the first frame update
+    void Start()
+    {
+        beg_door = transform.position.x - (transform.localScale.x / 2);
+        End_door = transform.position.x + (transform.localScale.x / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            Transform Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            if (beg_door < Player.position.x && Player.position.x < End_door)
+            {
+                Date.SetActive(true);
+            }
+            else
+            {
+                Date.SetActive(false);
+            }
+
+        }
+    }
+}
