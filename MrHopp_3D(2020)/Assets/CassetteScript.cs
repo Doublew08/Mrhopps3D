f691957a8da02e497c9bb14b5b6e9d4815a34640
Doublew08 <86314168+Doublew08@users.noreply.github.com>
2021-08-18 20:21:40 +0200

    مخم

--- MrHopp_3D(2020)/Assets/CassetteScript.cs
+++ MrHopp_3D(2020)/Assets/CassetteScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CassetteScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+    void start()
     {
-        
-    }
+        posOffset = transform.position;
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
     }
 }
