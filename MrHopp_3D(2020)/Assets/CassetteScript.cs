9723c86ac7cdf04a406acda74cf7cd8fb11ffc6e
Doublew08 <86314168+Doublew08@users.noreply.github.com>
2021-08-21 19:03:21 +0200

    Added Cassette

--- MrHopp_3D(2020)/Assets/CassetteScript.cs
+++ MrHopp_3D(2020)/Assets/CassetteScript.cs
@@ -4,20 +4,69 @@ using UnityEngine;
 
 public class CassetteScript : MonoBehaviour
 {
+
     public float degreesPerSecond = 15.0f;
     public float amplitude = 0.5f;
     public float frequency = 1f;
+    public GameObject Image;
+    public GameObject TapeImage;
+    //public int Room_num;
+    float beg_door;
+    float End_door;
+    public bool SeeingTheDrawing;
+
+
+    // Position Storage Variables
     Vector3 posOffset = new Vector3();
     Vector3 tempPos = new Vector3();
-    void start()
-    {
-        posOffset = transform.position;
 
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+        beg_door = transform.position.x - (transform.localScale.x / 2);
+        End_door = transform.position.x + (transform.localScale.x / 2);
     }
+
+    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
-        tempPos = posOffset;
-        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            Transform Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+
+            // Spin object around Y-Axis
+            transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+            // Float up/down with a Sin()
+            tempPos = posOffset;
+            tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+            transform.position = tempPos;
+            if (beg_door < Player.position.x && Player.position.x < End_door)
+            {
+                Image.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    
+                    SeeingTheDrawing = true;
+
+                    TapeImage.SetActive(true);
+                    PlayerMovement playerMovement = Player.GetComponent<PlayerMovement>();
+
+                    playerMovement.canmove = false;
+
+                }
+                
+            }
+            else
+            {
+                Image.SetActive(false);
+            }
+
+        }
     }
 }
+
