8da4c6ca1e57479896376d16256ffaacbda10a20
AhmedGamal227 <79380871+AhmedGamal227@users.noreply.github.com>
2021-08-23 01:00:46 +0200

    Finnnnnaaaalyyy(Stamina Fix) :))))))))))))))

---
+++ MrHopp_3D(2020)/Assets/SomeTestScripts/DecreaseTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecreaseTest : MonoBehaviour
+{
+    StaminaTest Stamina;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stamina = FindObjectOfType<StaminaTest>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(2F);
+            Stamina.DecreaseInStamina(10);
+        }
+    }
+}
---
+++ MrHopp_3D(2020)/Assets/SomeTestScripts/StaminaTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaTest : MonoBehaviour
+{
+    public Slider StaminaSlider;
+    public const float MaxStamina = 100f;
+    public float Stamina;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stamina = MaxStamina;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Stamina < MaxStamina)
+        {
+            IncreaseTheStamina();
+        }
+    }
+    public void DecreaseInStamina(float amount)
+    {
+        Stamina = Stamina - amount * 2;
+        StaminaSlider.value = Stamina;
+        if (Stamina < 0)
+        {
+            Stamina = 0;
+            Debug.Log("You dont have enough stamina");
+            
+        }
+    }
+    void IncreaseTheStamina()
+    {
+
+        Stamina = Stamina + Time.deltaTime * 4;
+        StaminaSlider.value = StaminaSlider.value + Time.deltaTime * 4;
+    }
+}
