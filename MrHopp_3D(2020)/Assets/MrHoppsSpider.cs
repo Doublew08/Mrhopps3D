398e0bcee1a691b278b54bf93152a59dcb38e91c
AhmedGamal227 <79380871+AhmedGamal227@users.noreply.github.com>
2021-09-13 00:11:39 +0200

    SpiderAnimation(Walk)100%

--- MrHopp_3D(2020)/Assets/MrHoppsSpider.cs
+++ MrHopp_3D(2020)/Assets/MrHoppsSpider.cs
@@ -7,15 +7,19 @@ public class MrHoppsSpider : MonoBehaviour
 {
     public GameObject Player;
     NavMeshAgent agent;
+    private Animator Rabbit;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        Rabbit = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(Player.transform.position);
+        
+
     }
 }
