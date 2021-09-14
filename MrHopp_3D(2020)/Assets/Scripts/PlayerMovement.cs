3a3045eae8b092fbdec3e7ca8464566cb512bde9
AhmedGamal227 <79380871+AhmedGamal227@users.noreply.github.com>
2021-09-15 00:17:28 +0200

    StaminaFIX(50%)

--- MrHopp_3D(2020)/Assets/Scripts/PlayerMovement.cs
+++ MrHopp_3D(2020)/Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 40f;
     public float jump = 50f;
     public float gravity = 300f;
-
+    public GameObject Player;
 
     float hor;
     float ver;
@@ -18,12 +18,13 @@ public class PlayerMovement : MonoBehaviour
     Vector3 movedire = Vector3.zero;
     Vector3 jumpdire = Vector3.zero;
     StaminaScript PlayerStamina;
-
+    Vector3 PlayerPos;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
         PlayerStamina = GetComponent<StaminaScript>();
+        PlayerPos = transform.position;
     }
 
     // Update is called once per frame
@@ -33,43 +34,54 @@ public class PlayerMovement : MonoBehaviour
         {
             move();
         }
-
+        if (Player.transform.position != PlayerPos)
+        {
+            Debug.Log("Player has moved");
+        }
+        else
+        {
+            Debug.Log("Player has not moved");
+        }
+        PlayerPos = Player.transform.position;
     }
 
     void move()
-    { 
+    {
         //make sure if player on the ground
         if (controller.isGrounded)
         {
-            //put the axises(horizontal and vertical in movedir vector)
-            movedire = new Vector3(Input.GetAxis("Horizontal"), .0f, Input.GetAxis("Vertical"));
-            //put speed to movedir and direction
-            movedire = movedire * speed;
-            movedire = transform.TransformDirection(movedire);
-            //check if user press jump button
-            if (Input.GetButton("Jump"))
-            {
-                //make the player jump
-                movedire.y = jump;
+           
+                //put the axises(horizontal and vertical in movedir vector)
+                movedire = new Vector3(Input.GetAxis("Horizontal"), .0f, Input.GetAxis("Vertical"));
+                //put speed to movedir and direction
+                movedire = movedire * speed;
+                movedire = transform.TransformDirection(movedire);
+                //check if user press jump button
+                if (Input.GetButton("Jump"))
+                {
+                    //make the player jump
+                    movedire.y = jump;
+                }
             }
-        }
-        //Check if the User press LeftShift
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            //Increse the Speed and the stamina will decrease
-            speed = 70f;
-            PlayerStamina.DecreaseInStamina(0.5f);
-        }
-        //Check if the player dont press LeftShift
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            //Decrease the speed and the stamina will not be decreased
-            speed = 40f;
-            PlayerStamina.DecreaseInStamina(0.0f);
-        }
-        //The gravity will decrease jump speed until the player be on the ground
-        movedire.y = movedire.y - gravity  * Time.deltaTime;
-        //make the player move
-        controller.Move(movedire * Time.deltaTime);
+            //Check if the User press LeftShift
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                //Increse the Speed and the stamina will decrease
+                speed = 70f;
+                PlayerStamina.DecreaseInStamina(0.5f);
+            }
+            //Check if the player dont press LeftShift
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                //Decrease the speed and the stamina will not be decreased
+                speed = 40f;
+                PlayerStamina.DecreaseInStamina(0.0f);
+            }
+            //The gravity will decrease jump speed until the player be on the ground
+            movedire.y = movedire.y - gravity * Time.deltaTime;
+            //make the player move
+            controller.Move(movedire * Time.deltaTime);
+              
     }
+    
 }
---
+++ MrHopp_3D(2020)/Assets/Scripts/RayCastRabbitFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastRabbitFollow : MonoBehaviour
+{
+    private WayPoints way;
+    // Start is called before the first frame update
+    void Start()
+    {
+        way = FindObjectOfType<WayPoints>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit ray;
+        if (Physics.Raycast(transform.position, -transform.up, out ray, Mathf.Infinity))
+        {
+            if (ray.collider.tag == "Player")
+            {
+                Debug.Log("Shit");
+                way.movementSpeed = 30f;
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, -transform.up * 1000);
+    }
+    IEnumerator DecreseTheSpeed()
+    {
+        yield return new WaitForSeconds(5);
+        way.movementSpeed = 20f;
+    }
+}
