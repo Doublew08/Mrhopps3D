aefd43173f452a87e8a4993672860b255f365a08
AhmedGamal227 <79380871+AhmedGamal227@users.noreply.github.com>
2021-09-22 00:14:25 +0200

    Door

---
+++ MrHopp_3D(2020)/Assets/Scripts/OpenDoorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorScript : MonoBehaviour
+{
+    [SerializeField] private bool OpenWay;
+    [SerializeField] private float OpenSpeed;
+
+    private bool IsOpen;
+    private float AngleSpeedTarget;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        OpenDoor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion smoothRotate = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, AngleSpeedTarget, 0)
+            , OpenSpeed * Time.deltaTime);
+        transform.localRotation = smoothRotate;
+    }
+    public void OpenDoor()
+    {
+        IsOpen = !IsOpen;
+        if (IsOpen)
+        {
+            if (OpenWay)
+            {
+                AngleSpeedTarget = 90f;
+            }
+            else
+            {
+                AngleSpeedTarget = -90f;
+            }
+        }
+        else
+        {
+            AngleSpeedTarget = 0f;
+        }
+    }
+}
